b7ee659a22cde4ed8907126aab3f5e2caa2ab40e
byte4u <77403094+byte4u@users.noreply.github.com>
2021-05-15 18:29:45 -0300

    Data structure

---
+++ Search Data/Search.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+/**
+ * Search data
+ * @author Samadhi
+ * @date 2021/05/15
+ */
+ 
+public class Search {
+
+	public static void search(int position){
+		String[] list = {"Bob", "Bruce", "Jennifer", "Kate", "Julie"};
+		System.out.println(list[position]);	
+	}
+    public static void main(String[] args) {
+        search(0);
+		///search(1);
+		//search(2);
+		//search(3);
+		//search(4);
+		//search(5);
+    }
+}
